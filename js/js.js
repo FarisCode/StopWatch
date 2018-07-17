d6582e12e2aa69ce139536b98f65ddc37771e679
farisSudofy <faris@sudofy.com>
2018-07-17 12:08:00 +0500

    Added Pause and Reset Functions and optimized code

--- js/js.js
+++ js/js.js
@@ -1,46 +1,78 @@
-var m="0000";
-var i=m[0],j=m[1],k=m[2],l=m[3];
-window.setInterval(timer, 1000);
+var m = "0000";
+var i = m[0], j = m[1], k = m[2], l = m[3];
+var flag = 0;
+var inter;
+function reset() {
+    i = m[0];
+    j = m[1];
+    k = m[2];
+    l = m[3];
+    inter;
+    post();
+    flag = 0;
+    window.clearInterval(inter);
+    unset("one");
+    unset("two");
+    unset("three");
+    unset("four");
+}
+function start() {
+    if (flag === 0) {
+        flag = 1;
+        inter = window.setInterval(timer, 1000);
+        document.getElementById("one").style.animationName = "timer";
+    } else {
+        unset("one");
+        unset("two");
+        unset("three");
+        unset("four");
+        flag = 0;
+        window.clearInterval(inter);
+    }
 
-function timer(){
+}
+
+function timer() {
     l++;
-    var blah;
-    if(l===1){
-        ele=document.getElementById("two");
-        ele.style.animationName="timers";
-        ele=document.getElementById("three");
-        ele.style.animationName="timers";
-        ele=document.getElementById("four");
-        ele.style.animationName="timers";
+    var ele;
+    if (l === 1) {
+        unset("two");
+        unset("three");
+        unset("four");
     }
-    if(l===10){
+    if (l === 10) {
         k++;
-        l=0;
-        ele=document.getElementById("two");
-        ele.style.animationName="timer";
+        l = 0;
+        ele = document.getElementById("two");
+        ele.style.animationName = "timer";
     }
-    if(k===10){
+    if (k === 10) {
         j++;
-        k=0;
-        ele=document.getElementById("three");
-        ele.style.animationName="timer";
+        k = 0;
+        ele = document.getElementById("three");
+        ele.style.animationName = "timer";
     }
-    if(j===10){
+    if (j === 10) {
         i++;
-        j=0;
-        ele=document.getElementById("four");
-        ele.style.animationName="timer";
+        j = 0;
+        ele = document.getElementById("four");
+        ele.style.animationName = "timer";
     }
-    if(i===10){
-        i=0;
-        j=0;
-        k=0;
-        l=0;
+    if (i === 10) {
+        i = 0;
+        j = 0;
+        k = 0;
+        l = 0;
     }
+    post();
+}
+function post() {
     document.getElementById("ths").innerHTML = i;
     document.getElementById("huns").innerHTML = j;
     document.getElementById("tens").innerHTML = k;
     document.getElementById("ones").innerHTML = l;
 }
+function unset(str) {
+    document.getElementById(str).style.animationName = "timers";
+}
 
-document.getElementById("one").style.animationName="timer";
